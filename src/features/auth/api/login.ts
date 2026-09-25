a985812782556d64a163dbbb962b9de720ba3e83
ErfanSadholly <erfanshahhosseini13@gmail.com>
2026-09-25 15:49:11 +0330

    fix

--- src/features/auth/api/login.ts
+++ src/features/auth/api/login.ts
@@ -5,7 +5,7 @@ import { useAuthStore } from "../store/auth-store";
 
 export async function Login(request: LoginRequest): Promise<LoginResponse> {
     const res: LoginResponse = {
-        accessToken: (await apiClient.post("auth/login", request)).data.data.AccessToken
+        accessToken: (await apiClient.post("auth/login", request)).data.data.accessToken
     }
     useAuthStore.getState().setAccessToken(res.accessToken)
     return res;
